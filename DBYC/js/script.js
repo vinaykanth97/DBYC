1667f54c025c8427087ee4445fa0f62f5f931877
vinay <31034875+vinaypump@users.noreply.github.com>
2021-01-26 18:01:05 +0530

    Add files via upload

--- DBYC/js/script.js
+++ DBYC/js/script.js
@@ -1,11 +1,28 @@
+let galleryTile = document.querySelectorAll(".outer");
+
 // Slider
-var swiper = new Swiper(".swiper-container", {
+var swiper = new Swiper(".infos .swiper-container", {
   flipEffect: {
     slideShadows: true,
   },
   navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    nextEl: ".swiper-button-next-unique",
+    prevEl: ".swiper-button-prev-unique",
+  },
+});
+
+// Autoplay
+var autoSwiper = new Swiper(".right-date .swiper-container", {
+  direction: "horizontal",
+  slidesPerView: 2,
+  centeredSlides: true,
+  loop: true,
+  freeMode: false,
+  speed: 4000,
+  autoHeight: true,
+  autoplay: {
+    delay: 0,
+    disableOnInteraction: false,
   },
 });
 // Tabs
@@ -14,18 +31,21 @@ let activeTabPos =
 document.querySelector(".arrow").style.top = `${activeTabPos}px`;
 let tabs = document.querySelectorAll(".tabs-sec li");
 let tabContent = document.querySelectorAll(".tab-content");
+let btn = document.querySelector(".btn");
 function selectItem(e, index) {
   RemoveActive();
   let getData = e.target.getAttribute("data-id");
-  document
-    .querySelector(`.overall-tabs li[data-id="${getData}"]`)
-    .classList.add("active");
+  let currElement = document.querySelector(
+    `.overall-tabs li[data-id="${getData}"]`
+  );
+  currElement.classList.add("active");
   let activeTabPos =
     document.querySelector(".overall-tabs li.active").offsetTop + 18;
   document.querySelector(".arrow").style.top = `${activeTabPos}px`;
   document
     .querySelector(`.tab-content[data-id="${getData}"]`)
     .classList.add("active");
+  btn.innerText = getData;
 }
 
 function RemoveActive() {
@@ -41,3 +61,27 @@ tabs.forEach((tab, index) =>
     selectItem(e, index);
   })
 );
+
+// Gallery Passage
+
+galleryTile.forEach((tile) => {
+  tile.addEventListener("click", function (e) {
+    let getData = this.getAttribute("data-id");
+    let outerGallery = document.querySelector(`.outer[data-id="${getData}"]`);
+    outerGallery.parentElement.style.display = "none";
+    document.querySelector(
+      `.inner-gallery .inner-pics[data-id="${getData}"]`
+    ).style.display = "block";
+  });
+});
+let backBtn = document.querySelectorAll(".back");
+backBtn.forEach((back, index) => {
+  back.addEventListener("click", function (e) {
+    let getData = this.getAttribute("data-id");
+    let outerGallery = document.querySelector(`.outer[data-id="${getData}"]`);
+    outerGallery.parentElement.style.display = "flex";
+    document.querySelector(
+      `.inner-gallery .inner-pics[data-id="${getData}"]`
+    ).style.display = "none";
+  });
+});
